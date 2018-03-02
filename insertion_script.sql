863c2e892f13d6d12740a8d71320a5a815cb7272
Gabriel Garcia <gabriel@wifi-83-231.uc3m.es>
2018-03-02 18:18:00 +0100

    Added Town

--- insertion_script.sql
+++ insertion_script.sql
@@ -1,3 +1,3 @@
-INSERT INTO OWNER (Name,Surname_1,Surname_2,Address,Dni,Mobile,Email,Birth)
-SELECT owner_name,owner_surn1,owner_surn2,owner_address,owner_DNI,TO_NUMBER(owner_mobile,'999999999'),owner_email,TO_CHAR(TO_DATE(owner_birth,'YYYY/MM/DD'),'YYYY/MM/DD')
+INSERT INTO OWNER (Name,Surname_1,Surname_2,Address,Town,Dni,Mobile,Email,Birth)
+SELECT owner_name,owner_surn1,owner_surn2,owner_address,owner_town,owner_DNI,TO_NUMBER(owner_mobile,'999999999'),owner_email,TO_DATE(owner_birth,'YYYY-MM-DD')
   FROM FSDB.MEGATABLE;
