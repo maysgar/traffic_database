6d07684bda28a1a1ea6159054a85ec04bb888ad1
Gabrielglbh <100346140@alumnos.uc3m.es>
2018-03-27 19:49:32 +0200

    probando funcion 3

--- Lab2/functions.sql
+++ Lab2/functions.sql
@@ -41,6 +41,8 @@ BEGIN
 
 END;
 
+--Es cumulativo, por cada observacion realizada en dicho radar ???
+
 /*
 PRUEBAS:
 
@@ -72,24 +74,78 @@ END; /
 
 -- Amount for a ‘safety distance’ radar sanction.
 
-CREATE OR REPLACE FUNCTION safety_distance ()
+CREATE OR REPLACE FUNCTION safety_distance (vehicle_input_1 VARCHAR2, vehicle_input_2 VARCHAR2,
+ road_input VARCHAR2, km_point_input NUMBER, direction_input VARCHAR2)
 RETURN NUMBER
 IS
+  date_1 VARCHAR2(28);
+  date_2 VARCHAR2(28);
+  distance NUMBER(3);
+  time_elapsed INTEGER := 0;
+  total_amount NUMBER(4);
+  boolean_aux INTEGER := 0;
+  amount_fine INTEGER := 10;
+
+  CURSOR vehicle_to_be_fined (vehicle_input_1 VARCHAR2, vehicle_input_2 VARCHAR2, road_input VARCHAR2,
+   km_point_input NUMBER, direction_input VARCHAR2) IS
+    SELECT nPlate, speed, road, speed_limit, km_point, direction, odatetime
+    FROM OBSERVATIONS a JOIN ROADS b
+    ON a.road = b.name
+    WHERE nPlate = vehicle_input_1 AND road = road_input AND km_point = km_point_input
+      AND direction = direction_input;
+
+  CURSOR vehicle_2 (vehicle_input_1 VARCHAR2, vehicle_input_2 VARCHAR2, road_input VARCHAR2,
+   km_point_input NUMBER, direction_input VARCHAR2) IS
+    SELECT nPlate, speed, road, speed_limit, km_point, direction, odatetime
+    FROM OBSERVATIONS a JOIN ROADS b
+    ON a.road = b.name
+    WHERE nPlate = vehicle_input_2 AND road = road_input AND km_point = km_point_input
+      AND direction = direction_input;
+
 BEGIN
+    IF vehicle_to_be_fined %ISOPEN THEN
+      CLOSE vehicle_1;
+    END IF;
+    IF vehicle_2 %ISOPEN THEN
+      CLOSE vehicle_2;
+    END IF;
+
+    FOR i IN vehicle_to_be_fined(vehicle_input_1,vehicle_input_2,road_input,km_point_input,direction_input)
+    LOOP
+      date_1 := CAST(i.odatetime AS VARCHAR2(28));
+      FOR i IN vehicle_2(vehicle_input_1,vehicle_input_2,road_input,km_point_input,direction_input)
+      LOOP
+        date_2 := CAST(i.odatetime AS VARCHAR2(28));
+        --calculo de la diferencia entre observaciones
+        time_elapsed := TO_NUMBER(TO_DATE(date_1, 'SS.FF')) - TO_NUMBER(TO_DATE(date_2, 'SS.FF'));
+        IF TO_DATE(date_1, 'MM-DD-YY') = TO_DATE(date_2, 'MM-DD-YY') AND
+           THEN
+          boolean_aux := 1;
+          time_elapsed := ABS(3.6-time_elapsed);
+          EXIT WHEN boolean_aux = 1;
+        END IF;
+      END LOOP;
+      EXIT WHEN boolean_aux = 1;
+    END LOOP;
+
+    --ACCESS HERE WHEN THE time_elapsed HAS BEEN CALCULATED AND THE EXIT COMMAND
+    --HAS BEEN EXECUTED.
+    total_amount := CEIL(time_elapsed*amount_fine);
+    BMS_OUTPUT.PUT_LINE(total_amount);
+    RETURN total_amount;
+
 END; /
 
 -- Observation immediately prior to a given observation (of the same radar)
 
 CREATE OR REPLACE FUNCTION obs_right_after_radar ()
-RETURN
-IS
+RETURN OBSERVATIONS%ROWTYPE AS obs_row_f1%ROWTYPE;
 BEGIN
 END; /
 
 -- Observation immediately prior to a given observation (of the same vehicle)
 
 CREATE OR REPLACE FUNCTION obs_right_after_vehicle ()
-RETURN
-IS
+RETURN OBSERVATIONS%ROWTYPE AS obs_row_f2%ROWTYPE;
 BEGIN
 END; /
