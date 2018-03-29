f6ef6551f92e4c4e062325782965bbfca50860ea
Gabrielglbh <100346140@alumnos.uc3m.es>
2018-03-29 16:33:31 +0200

    View 1 √

--- Lab2/views.sql
+++ Lab2/views.sql
@@ -5,19 +5,37 @@ Do a view that provides the infringing vehicles, the date, and the speed differe
 with the legal margin, to support the analysis of the convenience of this norm. */
 
 CREATE OR REPLACE VIEW sanction_low_speed AS
-SELECT nPlate, odatetime, ((speedlim/2) - speed) AS speed_difference
-FROM(
-    SELECT road, Km_point, direction FROM OBSERVATIONS A
-    FULL OUTER JOIN
-    SELECT road, km_point, direction FROM RADARS B
-    ON A.road = B.road, A.Km_point = B.Km_point, A.direction = B.direction
-)
-WHERE speed <= (speedlim/2);
+  SELECT nPlate,odatetime,difference FROM(
+    SELECT nPlate,speed,odatetime,speed_limit/2-speed AS difference FROM
+    OBSERVATIONS NATURAL JOIN RADARS JOIN ROADS ON road = name
+    WHERE speed_limit/2 >= speed)
+  WHERE difference > 0;
+
+/*
+  Pruebas: --34 rows--
+
+  SELECT nPlate,speed,road,speed_limit,difference FROM(
+    SELECT nPlate,speed,speed,road,speed_limit,speed_limit/2-speed AS difference FROM
+    OBSERVATIONS NATURAL JOIN RADARS JOIN ROADS ON road = name
+    WHERE speed_limit/2 >= speed)
+  WHERE difference > 0;
+
+  Observing the speed of the vehicle and the speed limit of the road is easy to
+  verify the correct functioning of the query.
+  All cars in the view are below the speed_limit of the road.
+
+    nPlate = 7283OOE | 3797UOE
+    speed = 31 km/h | 23 km/h
+    road = M30 | M30
+    speed_limit(road) = 70 km/h | 70 km/h
+    difference = 4 km/h | 12 km/h
+    ok - If the reglamentary difference on speed is 0, the observation is neglected.
+*/
 
 /* b) Monthly Whinger: drivers with more allegations rejected for each month. */
 CREATE OR REPLACE VIEW monthly_whinger AS
 SELECT debtor, EXTRACT(MONTH from reg_date) AS month, COUNT(*) AS allegations
-FROM( 
+FROM(
     SELECT obs1_veh, obs1_date, tik_type FROM TICKETS AS A
     FULL OUTER JOIN
     SELECT obs1_veh, obs1_date, tik_type FROM ALLEGATIONS AS B
@@ -46,5 +64,5 @@ of the road without exceeding it. Tip: base the calculation on the percentage of
 the maximum speed that each observation records. */
 CREATE OR REPLACE VIEW quickWitted_drivers AS
 SELECT DNI
-FROM 
-WHERE ROWNUM = 10; 
+FROM
+WHERE ROWNUM = 10;
