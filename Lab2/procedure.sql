a6b8fed02cacc34d5e2a9d88f3c34e29495292d1
Gabrielglbh <100346140@alumnos.uc3m.es>
2018-03-29 18:04:46 +0200

    YO NO SE QUE PASA

--- Lab2/procedure.sql
+++ Lab2/procedure.sql
@@ -1,12 +1,12 @@
 --Daily sanctions
 
-CREATE OR REPLACE PROCEDURE daily_sanctions(today DATE) IS
+CREATE OR REPLACE PROCEDURE daily_sanctions IS
 
   total_amount INTEGER := 0;
 
-  CURSOR fines(today DATE) IS
-    SELECT nPlate, dni, odatetime, speed, road, speed_limit, km_point, direction
-    FROM RADARS JOIN OBSERVATIONS ON road = name
+  CURSOR fines IS
+    SELECT nPlate, owner, odatetime, speed, road, speed_limit, km_point, direction
+    FROM RADARS a JOIN OBSERVATIONS b ON a.road = b.road
     NATURAL JOIN VEHICLES JOIN PERSONS ON owner = dni;
 
 BEGIN
@@ -14,13 +14,14 @@ BEGIN
       CLOSE fines;
     END IF;
 
-    FOR i IN fines(today)
+    FOR i IN fines
     LOOP
       --calling function already created--
       total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction);
       IF total_amount > 0 THEN
         --Revisar bien todos los campos
-        INSERT INTO TICKETS VALUES(i.nPlate,,i.odatetime,'','','',sysdate,sysdate+20,'',total_amount,i.dni,'R');
+        INSERT INTO TICKETS VALUES(i.nPlate,i.odatetime,'','','',sysdate,sysdate+20,'',total_amount,i.dni,'R');
       END IF;
+      total_amount := 0;
     END LOOP;
 END;
