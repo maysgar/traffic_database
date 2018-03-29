9c2b76ca2830ec94fc4eff1648fdf5d2f4567464
Gabrielglbh <100346140@alumnos.uc3m.es>
2018-03-29 17:33:30 +0200

    probando procedure

--- Lab2/procedure.sql
+++ Lab2/procedure.sql
@@ -1,1 +1,26 @@
 --Daily sanctions
+
+CREATE OR REPLACE PROCEDURE daily_sanctions(today DATE) IS
+
+  total_amount INTEGER := 0;
+
+  CURSOR fines(today DATE) IS
+    SELECT nPlate, dni, odatetime, speed, road, speed_limit, km_point, direction
+    FROM RADARS JOIN OBSERVATIONS ON road = name
+    NATURAL JOIN VEHICLES JOIN PERSONS ON owner = dni;
+
+BEGIN
+    IF fines %ISOPEN THEN
+      CLOSE fines;
+    END IF;
+
+    FOR i IN fines(today)
+    LOOP
+      --calling function already created--
+      total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction);
+      IF total_amount > 0 THEN
+        --Revisar bien todos los campos
+        INSERT INTO TICKETS VALUES(i.nPlate,,i.odatetime,'','','',sysdate,sysdate+20,'',total_amount,i.dni,'R');
+      END IF;
+    END LOOP;
+END;
