44425ef98f8245f19f955d7f41a0c68999df7a6c
Ismael Garrido <100346215@alumnos.uc3m.es>
2018-03-30 11:51:49 +0200

    mierdas de isma

--- Lab2/procedure.sql
+++ Lab2/procedure.sql
@@ -40,3 +40,39 @@ BEGIN
       total_amount := 0;
     END LOOP;
 END daily_sanctions;
+
+
+
+
+
+
+
+
+
+--mierdas de isma--
+
+CREATE OR REPLACE PROCEDURE daily_sanctions IS
+
+  total_amount INTEGER := 0;
+
+  CURSOR fines IS
+    SELECT nPlate, owner, odatetime, speed, road, speedlim, Km_point, direction
+    FROM RADARS a JOIN OBSERVATIONS b ON a.road = b.road AND a.Km_point = b.Km_point AND a.direction = b.direction 
+    NATURAL JOIN VEHICLES JOIN PERSONS ON owner = dni;
+
+BEGIN
+    IF fines %ISOPEN THEN
+      CLOSE fines;
+    END IF;
+
+    FOR i IN fines
+    LOOP
+      --calling function already created--
+      total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction);
+      IF total_amount > 0 THEN
+        --Revisar bien todos los campos
+        INSERT INTO TICKETS VALUES(i.nPlate,i.odatetime,'','','',sysdate,sysdate+20,'',total_amount,i.dni,'R');
+      END IF;
+      total_amount := 0;
+    END LOOP;
+END daily_sanctions;
