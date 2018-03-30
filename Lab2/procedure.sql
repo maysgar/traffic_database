52e8b63706a9b55491725f1387c881a0b81d90fc
Gabrielglbh <100346140@alumnos.uc3m.es>
2018-03-30 15:04:05 +0200

    Ya por hoy termino, mira coments

--- Lab2/procedure.sql
+++ Lab2/procedure.sql
@@ -1,16 +1,16 @@
 --Daily sanctions
 /*
 whenever the car speed is higher than allowed, or when we acquire two observations
-of the same vehicle on the same road in different mile markers more distant than 
-what can be legally covered in that lapse of time, or even when the safety distance 
+of the same vehicle on the same road in different mile markers more distant than
+what can be legally covered in that lapse of time, or even when the safety distance
 is not observed.
-The amount of the fine is 10 € for each km/h above the speed limit (rounded 
-high). The minimum safety distance is one meter for every km/h of 
-speed. Safety lapse is the time required to cover the safety distance (regardless of 
-the speed, the time between any two vehicles has to be equal or above 3.6 seconds). 
-Therefore, it is considered that any vehicle does not observe the safety distance if 
-there is a previous observation of the same radar in less than that time. The fine 
-is 10€ for each tenth of a second less than the minimum during this period (rounded 
+The amount of the fine is 10 € for each km/h above the speed limit (rounded
+high). The minimum safety distance is one meter for every km/h of
+speed. Safety lapse is the time required to cover the safety distance (regardless of
+the speed, the time between any two vehicles has to be equal or above 3.6 seconds).
+Therefore, it is considered that any vehicle does not observe the safety distance if
+there is a previous observation of the same radar in less than that time. The fine
+is 10€ for each tenth of a second less than the minimum during this period (rounded
 high).
 and the penalty applied if the payment fulfilled later than due (the fine is doubled).
 */
@@ -32,23 +32,19 @@ BEGIN
     FOR i IN fines
     LOOP
       --calling function already created--
-      total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction);
+      total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction,i.odatetime);
       IF total_amount > 0 THEN
-        --Revisar bien todos los campos
         INSERT INTO TICKETS VALUES(i.nPlate,i.odatetime,'','','',sysdate,sysdate+20,'',total_amount,i.dni,'R');
       END IF;
       total_amount := 0;
     END LOOP;
 END daily_sanctions;
-
-
-
-
-
-
-
-
-
+/******************************************************************************/
+/*
+Va a haber peleas porque deberemos de ir acumulando la cantidad en dinero
+  de cada owner
+*/
+/******************************************************************************/
 --mierdas de isma--
 
 CREATE OR REPLACE PROCEDURE daily_sanctions IS
@@ -57,7 +53,7 @@ CREATE OR REPLACE PROCEDURE daily_sanctions IS
 
   CURSOR fines IS
     SELECT nPlate, owner, odatetime, speed, road, speedlim, Km_point, direction
-    FROM RADARS a JOIN OBSERVATIONS b ON a.road = b.road AND a.Km_point = b.Km_point AND a.direction = b.direction 
+    FROM RADARS a JOIN OBSERVATIONS b ON a.road = b.road AND a.Km_point = b.Km_point AND a.direction = b.direction
     NATURAL JOIN VEHICLES JOIN PERSONS ON owner = dni;
 
 BEGIN
@@ -68,7 +64,7 @@ BEGIN
     FOR i IN fines
     LOOP
       --calling function already created--
-      total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction);
+      total_amount := exceeding_max_speed(i.nPlate,i.road,i.km_point,i.direction,i.odatetime);
       IF total_amount > 0 THEN
         --Revisar bien todos los campos
         INSERT INTO TICKETS VALUES(i.nPlate,i.odatetime,'','','',sysdate,sysdate+20,'',total_amount,i.dni,'R');
