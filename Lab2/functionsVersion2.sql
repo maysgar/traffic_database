37535245758a4769b86e95d19eed197444a7e227
Gabrielglbh <100346140@alumnos.uc3m.es>
2018-04-04 19:00:35 +0200

    Peleotes con funciones B y C

--- Lab2/functionsVersion2.sql
+++ Lab2/functionsVersion2.sql
@@ -102,21 +102,57 @@ Results expected:
 CREATE OR REPLACE FUNCTION exceeding_section_speed (obs OBSERVATIONS%ROWTYPE)
 RETURN NUMBER
 IS
+  obs2 OBSERVATIONS%ROWTYPE;
 BEGIN
---Call of obs_right_after_vehicle
+    obs2 := obs_right_after_vehicle(obs);
+    --If we are guaranteed to be on a section...
+    IF obs.km_point < obs2.km_point THEN
+      --And if we the observations were made in the same road and direction...
+      IF obs.road = obs2.road AND obs.direction = obs2.direction THEN
+        --In the same day, month, year and hour...
+        IF TO_CHAR(obs.odatetime,'DD-MM-YY HH24') = TO_CHAR(obs2.odatetime,'DD-MM-YY HH24')
+          IF obs.speed > obs.speedlim
+        END IF;
+      END IF;
+    END IF;
 END;
 
 /*
 PRUEBAS:
 
+declare
+  a OBSERVATIONS%ROWTYPE;
+  result number;
+begin
+  a.nPlate := '7919AEO';
+  a.speed := 147;
+  a.road := 'M50';
+  a.direction := 'ASC';
+  a.km_point := 75;
+  a.odatetime := TO_TIMESTAMP('2010-09-03 23.24.33.540000','YYYY-MM-DD HH24.MI.SS.FF');
+  result := exceeding_max_speed(a);
+end;
 */
 
 -- Amount for a ‘safety distance’ radar sanction.
 CREATE OR REPLACE FUNCTION safety_distance (obs OBSERVATIONS%ROWTYPE)
 RETURN NUMBER
 IS
+  obs2 OBSERVATIONS%ROWTYPE;
+  partial_amount FLOAT := 0;
+  total_amount FLOAT := 0;
+  amount_fine FLOAT := 0;
+  time_elapsed FLOAT := 0;
 BEGIN
---Call of obs_right_after_radar
+    obs2 := obs_right_after_radar(obs);
+    time_elapsed := ABS(TO_NUMBER(EXTRACT(SECOND FROM obs.odatetime)) - TO_NUMBER(EXTRACT(SECOND FROM obs2.odatetime)));
+    IF time_elapsed < 3.6 AND TO_CHAR(obs.odatetime,'MM-DD-YY HH24.MI') = TO_CHAR(obs2.odatetime,'MM-DD-YY HH24.MI') THEN
+      partial_amount := 3.6-time_elapsed;
+    END IF;
+
+    total_amount := CEIL(partial_amount*amount_fine);
+    DBMS_OUTPUT.PUT_LINE(total_amount);
+    RETURN total_amount;
 END;
 
 /*
